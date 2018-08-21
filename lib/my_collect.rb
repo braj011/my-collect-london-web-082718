1148d78ee7e5e17aa69ad84005ce8198eb2f30a7
braj011 <bhagyaraj01@gmail.com>
2018-08-21 20:15:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(words) # put argument(s) here
   i = 0
   new_array = []
   while i < words.length
-    new_array << yield words[i]
+    new_array << yield(words[i])
     i += 1
   end
   new_array
